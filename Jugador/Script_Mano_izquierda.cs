a5e4452da4a3bb020b54f4760db9f4d5bf021bb8
10Myr10 <iperezfc1@gmail.com>
2020-06-08 13:59:02 +0200

    Estructurado contenido en carpetas

---
+++ Jugador/Script_Mano_izquierda.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mano_izquierda : MonoBehaviour
+{
+
+    protected Joystick joystick;
+    private Animator anim;
+    private Rigidbody2D rb2d;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.SetInteger("direction", this.transform.parent.gameObject.GetComponent<Animator>().GetInteger("direction"));
+        if (this.transform.parent.gameObject.GetComponent<Animator>().GetBool("shoot")) anim.SetInteger("direction", anim.GetInteger("direction") + 10);
+        anim.SetBool("walk", this.transform.parent.gameObject.GetComponent<Animator>().GetBool("walk"));
+     
+    }
+}
